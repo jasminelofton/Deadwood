8e78833ddb4a400add2b37d2ad1ed371b34b46a3
Jasmine Lofton <loftonjaz@gmail.com>
2026-05-07 08:13:16 -0700

    actually fixed now

--- Deadwood.java
+++ Deadwood.java
@@ -1,18 +1,16 @@
 /*
 
 
-*/
-
-public class Deadwood {
+*/public class Deadwood {
     public static void main(String[] args) {
 
         Moderator moderator = new Moderator();
-       
-        View view = new view();
-
-        Controller controller = new Controller();
+        View view = new View();
+        Controller controller = new Controller(moderator, view);
 
         view.setController(controller);
-
+        moderator.setController(controller);
+        
+        controller.startGame();
     }
 }
