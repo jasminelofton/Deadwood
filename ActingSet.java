7530155447c515e39ff72e78377d6e9f26106d5d
Austin Onnsy <onnsya@wwu.edu>
2026-05-11 03:23:14 -0700

    some comments and general bases, not yet to minimum

--- ActingSet.java
+++ ActingSet.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ActingSet extends Room {
+    private int counters;
+    private ArrayList<Dice> die = new ArrayList<Dice>();
 
     public ActingSet (String name) {
         super(name);
+    }
+
+    // need to figure what this does
+    private static int roleDice(){
+
+    }
+
+    // aware what this does, has general base 
+    public int[] Act(Role role){
+        counters--;
+
+    }
+
+    // base
+    private int[] wrapScene(){
+
     }
 }
--- CastingOffice.java
+++ CastingOffice.java
@@ -1,6 +1,19 @@
-public class CastingOffice extends Room {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class CastingOffice extends Room {
+    private String instructions;
+    List<Integer> ranks;
+    List<Integer> moneyPrices;
+    List<Integer> creditPrices;
+
+    
     public CastingOffice(String name) {
         super(name);
+
+        this.ranks = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6));
+        this.moneyPrices = new ArrayList<>(Arrays.asList(4, 10, 18, 28, 40));
+        this.creditPrices = new ArrayList<>(Arrays.asList(5, 10, 15, 20, 25));
     }
 }
